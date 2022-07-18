277d7d0476f4c897813abed472c7f10efad3c22e
Robert Shepley <shepleysound@gmail.com>
2022-07-18 15:57:12 -0700

    Separate stores into different object literals.

--- js/app.js
+++ js/app.js
@@ -15,10 +15,11 @@ function timeTranslate(hourInt) {
   }
 }
 
-const store = {
-  minCustomers: 0,
-  maxCustomers: 100,
-  avgCookiesPerCustomer: 4.3,
+const seattleStore = {
+  storeName: 'Seattle',
+  minCustomers: 23,
+  maxCustomers: 65,
+  avgCookiesPerCustomer: 6.3,
   randomCustomerCount() {
     // Inclusive random integer. Algorithm from MDN Math.random() docs.
     const min = Math.ceil(this.minCustomers);
@@ -35,11 +36,124 @@ const store = {
     for (let i = this.openingHour; i <= this.closingHour; i++) {
       const cookiesSold = Math.ceil(this.randomCustomerCount() * this.avgCookiesPerCustomer);
       this.hourlySalesArray.push(cookiesSold);
+      this.totalSales += cookiesSold;
     }
   }
 };
 
-store.simulateSales();
+const tokyoStore = {
+  storeName: 'Tokyo',
+  minCustomers: 3,
+  maxCustomers: 24,
+  avgCookiesPerCustomer: 1.2,
+  randomCustomerCount() {
+    // Inclusive random integer. Algorithm from MDN Math.random() docs.
+    const min = Math.ceil(this.minCustomers);
+    const max = Math.floor(this.maxCustomers);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+  openingHour: 6,
+  closingHour: 19,
+  hourlySalesArray: [],
+  totalSales: 0,
+  simulateSales() {
+    this.hourlySalesArray = [];
+    this.totalSales = 0;
+    for (let i = this.openingHour; i <= this.closingHour; i++) {
+      const cookiesSold = Math.ceil(this.randomCustomerCount() * this.avgCookiesPerCustomer);
+      this.hourlySalesArray.push(cookiesSold);
+      this.totalSales += cookiesSold;
+    }
+  }
+};
+
+const dubaiStore = {
+  storeName: 'Dubai',
+  minCustomers: 11,
+  maxCustomers: 38,
+  avgCookiesPerCustomer: 3.7,
+  randomCustomerCount() {
+    // Inclusive random integer. Algorithm from MDN Math.random() docs.
+    const min = Math.ceil(this.minCustomers);
+    const max = Math.floor(this.maxCustomers);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+  openingHour: 6,
+  closingHour: 19,
+  hourlySalesArray: [],
+  totalSales: 0,
+  simulateSales() {
+    this.hourlySalesArray = [];
+    this.totalSales = 0;
+    for (let i = this.openingHour; i <= this.closingHour; i++) {
+      const cookiesSold = Math.ceil(this.randomCustomerCount() * this.avgCookiesPerCustomer);
+      this.hourlySalesArray.push(cookiesSold);
+      this.totalSales += cookiesSold;
+    }
+  }
+};
+
+const parisStore = {
+  storeName: 'Paris',
+  minCustomers: 20,
+  maxCustomers: 38,
+  avgCookiesPerCustomer: 2.3,
+  randomCustomerCount() {
+    // Inclusive random integer. Algorithm from MDN Math.random() docs.
+    const min = Math.ceil(this.minCustomers);
+    const max = Math.floor(this.maxCustomers);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+  openingHour: 6,
+  closingHour: 19,
+  hourlySalesArray: [],
+  totalSales: 0,
+  simulateSales() {
+    this.hourlySalesArray = [];
+    this.totalSales = 0;
+    for (let i = this.openingHour; i <= this.closingHour; i++) {
+      const cookiesSold = Math.ceil(this.randomCustomerCount() * this.avgCookiesPerCustomer);
+      this.hourlySalesArray.push(cookiesSold);
+      this.totalSales += cookiesSold;
+    }
+  }
+};
+
+const limaStore = {
+  storeName: 'Lima',
+  minCustomers: 2,
+  maxCustomers: 16,
+  avgCookiesPerCustomer: 4.6,
+  randomCustomerCount() {
+    // Inclusive random integer. Algorithm from MDN Math.random() docs.
+    const min = Math.ceil(this.minCustomers);
+    const max = Math.floor(this.maxCustomers);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+  openingHour: 6,
+  closingHour: 19,
+  hourlySalesArray: [],
+  totalSales: 0,
+  simulateSales() {
+    this.hourlySalesArray = [];
+    this.totalSales = 0;
+    for (let i = this.openingHour; i <= this.closingHour; i++) {
+      const cookiesSold = Math.ceil(this.randomCustomerCount() * this.avgCookiesPerCustomer);
+      this.hourlySalesArray.push(cookiesSold);
+      this.totalSales += cookiesSold;
+    }
+  }
+};
+
+const storesArray = [seattleStore, tokyoStore, dubaiStore, parisStore, limaStore];
+storesArray.forEach(store => {
+  console.log(store.storeName);
+  store.simulateSales();
+  store.hourlySalesArray.forEach((sale, hour) => {
+    console.log(`${timeTranslate(hour + store.openingHour)}: ${sale} Cookies`);
+  });
+  console.log(`Total Sales: ${store.totalSales}`)
+});
 
 
 // Constructor function for Stores
