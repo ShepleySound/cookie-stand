981033e68b07a17dc9ae4d1f8c3dd5dadfb52ee8
Robert Shepley <shepleysound@gmail.com>
2022-07-18 15:39:21 -0700

    Flesh out store object.

--- js/app.js
+++ js/app.js
@@ -14,21 +14,56 @@ function timeTranslate(hourInt) {
     return `${hourInt - 12}PM`;
   }
 }
+
 const store = {
   minCustomers: 0,
   maxCustomers: 100,
-  averageCookiesPerCustomer: 4,
+  avgCookiesPerCustomer: 4.3,
   randomCustomerCount() {
     // Inclusive random integer. Algorithm from MDN Math.random() docs.
     const min = Math.ceil(this.minCustomers);
     const max = Math.floor(this.maxCustomers);
     return Math.floor(Math.random() * (max - min + 1) + min);
   },
-
+  openingHour: 6,
+  closingHour: 19,
+  hourlySalesArray: [],
+  totalSales: 0,
+  simulateSales() {
+    this.hourlySalesArray = [];
+    this.totalSales = 0;
+    for (let i = this.openingHour; i <= this.closingHour; i++) {
+      const cookiesSold = Math.ceil(this.randomCustomerCount() * this.avgCookiesPerCustomer);
+      this.hourlySalesArray.push(cookiesSold);
+    }
+  }
 };
 
-console.log(store.randomCustomerCount());
-console.log(timeTranslate(0));
-console.log(timeTranslate(12));
-console.log(timeTranslate(10));
-console.log(timeTranslate(23));
+store.simulateSales();
+
+
+// Constructor function for Stores
+// function Store(minCustomers, maxCustomers, avgCookiesPerCustomer) {
+//   this.minCustomers = minCustomers;
+//   this.maxCustomers = maxCustomers;
+//   this.avgCookiesPerCustomer = avgCookiesPerCustomer;
+//   this.randomCustomerCount = function() {
+//     // Inclusive random integer. Algorithm from MDN Math.random() docs.
+//     const min = Math.ceil(this.minCustomers);
+//     const max = Math.floor(this.maxCustomers);
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+//   };
+//   this.openingHour = 6;
+//   this.closingHour = 19;
+//   this.hourlySalesArray = [];
+//   this.totalSales = 0;
+//   this.simulateSales = function() {
+//     this.hourlySalesArray = [];
+//     this.totalSales = 0;
+//     for (let i = this.openingHour; i <= this.closingHour; i++) {
+//       const cookiesSold = Math.ceil(this.randomCustomerCount() * this.avgCookiesPerCustomer);
+//       this.hourlySalesArray.push(cookiesSold);
+//       this.totalSales += cookiesSold;
+//     }
+//   }
+// };
